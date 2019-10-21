bd7e26230d010f94aa6e6af305eeebd91ce95da3
thorcc <thorchrcow@gmail.com>
2019-10-21 19:01:04 +0200

    close navbar links when clicking on main

--- js/nav.js
+++ js/nav.js
@@ -2,3 +2,8 @@ function openNav() {
   const links = document.querySelector('.navLinks');
   links.style.display === 'flex' ? links.style.display = 'none' : links.style.display = 'flex';
 }
+
+function closeNav(){
+  const links = document.querySelector('.navLinks');
+  links.style.display = 'none';
+}
